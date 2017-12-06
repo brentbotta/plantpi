b7a43bf7b7b60e206144a28b26ae977c992b6c59
Brent Botta <brent.botta@gmail.com>
2017-12-05 17:41:03 -0800

    MySQLdb connection and write

--- plantpi/MySQLdb connect and write.py
+++ plantpi/MySQLdb connect and write.py
@@ -4,6 +4,7 @@ cnx = mysql.connector.connect(user='pi', password='#Bl0nd13007',
                               host='127.0.0.1',
                               database='plantpi')
 cursor = cnx.cursor()
+#Need to update the values below and support autonumbering
 add_temp = ("INSERT INTO temp (idtemp, temp) VALUES (2,13)")
 cursor.execute(add_temp)
 cnx.commit()
