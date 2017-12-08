6c5ae99a00e7c80849055d4733ee52a7072293fb
Brent Botta <brent.botta@gmail.com>
2017-12-08 10:22:13 -0800

    code cleanup

--- plantpi/GPIO Sample.py
+++ plantpi/GPIO Sample.py
@@ -1,15 +1,15 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO, time
 
 ledChannel = 13
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 #set channel to board number
 #GPIO.setup(channel, GPIO.IN)
 GPIO.setup(ledChannel, GPIO.OUT)
 
-GPIO.setup(ledChannel, GPIO.OUT, initial=GPIO.LOW)
+GPIO.output(ledChannel, False)
 print("Channel 13 set to LOW")
 
 #read value of channel
